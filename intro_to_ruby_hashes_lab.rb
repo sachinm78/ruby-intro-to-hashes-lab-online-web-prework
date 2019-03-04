caec2b7d6c2f27ec337667947bdebe47a1f1a255
Sachin Mahajan <sachinm78@gmail.com>
2019-03-04 18:09:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,11 +25,6 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-	
-		
-end
-
-def monopoly_with_fourth_tier
 	monopoly = {
 		:railroads =>
 		{
@@ -50,6 +45,10 @@ def monopoly_with_fourth_tier
 		}
 		}
 	}
+end
+
+def monopoly_with_fourth_tier
+	
 end
 
 
