cab63aa781d76fa3e9dcfb200fc6339a6da75ff1
Sachin Mahajan <sachinm78@gmail.com>
2019-03-04 18:04:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,13 +9,16 @@ end
 
 def monopoly
 	monopoly = {
-		:railroads=>{}
+		:railroads => {}
 	}
 end
 
 def monopoly_with_second_tier
 		monopoly = {
-		:railroads=>{}
+		:railroads =>
+		{
+		  :pieces => 4
+		  		}
 	}
 end
 
